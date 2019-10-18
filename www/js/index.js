f25c5d0fe667bfd0ab83407f6935c35701390128
matisslavrinovics98 <56152136+matisslavrinovics98@users.noreply.github.com>
2019-10-18 09:29:37 +0100

    Update index.js
    name

--- www/js/index.js
+++ www/js/index.js
@@ -6,7 +6,7 @@ document.addEventListener("deviceready", onDeviceReady, false);
 		
 	
 function updateDisplay() {
-	$("#launched").text("Application launched: " + launched_count);
+	$("#launched").text("Launched: " + launched_count);
 	$("#resumed").text("Application paused: " + paused_count);
 	$("#paused").text("Application resumed: " + resumed_count);
 }
